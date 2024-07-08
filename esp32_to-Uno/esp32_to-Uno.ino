9fc97c6026cae1d3a85c64ca4c71246e8055e706
HinnMachache <bennyhinn253.bh@gmail.com>
2024-07-08 15:25:02 +0300

    Serializing Data to JSON for data transfer

--- esp32_to-Uno/esp32_to-Uno.ino
+++ esp32_to-Uno/esp32_to-Uno.ino
@@ -1,6 +1,23 @@
 //Esp32 Code
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+#define WIFI_SSID "GW_Ki-Near"
+#define WIFI_PASSWORD "IMachache@123"
+#define API_KEY "AIzaSyAY5hEfz5BOqqGRLM0tp7Z8BoQcfi8HFJE"
+#define DATABASE_URL "https://esp32-project00-default-rtdb.firebaseio.com/"
+
 #define RXD2 16
 #define TXD2 17
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
 
 char dataIn;
 String message;
@@ -8,6 +25,10 @@ String message;
 void setup() {
   Serial.begin(19200);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  
+  wifi_connect();
+  firebase_auth();
+
 }
 
 void loop() {
@@ -27,10 +48,52 @@ void loop() {
 
   if (dataIn == '\n') {
     Serial.println(message);
+    if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0)) {
+      sendDataPrevMillis = millis();
+      if(Firebase.RTDB.setString(&fbdo, "ParkingInfo/Availabilty", WIFI_SSID)) {
+        Serial.println();
+        Serial.print(" - Succesfully saved to: " + fbdo.dataPath());
+        Serial.println(" (" + fbdo.dataType() + ")");
+      }else {
+        Serial.println("FAILED: " + fbdo.errorReason());
+      }
+    }
     
     dataIn = 0;
     message = "";
   }
-  delay(100);
+  // delay(100);
   
+}
+
+void wifi_connect() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void firebase_auth () {
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("SignUp Successful");
+    signupOK = true;
+  }else {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
--- ultrasonicDetection/ultrasonicDetection.ino
+++ ultrasonicDetection/ultrasonicDetection.ino
@@ -1,5 +1,5 @@
 #include <SoftwareSerial.h>
-
+#include <ArduinoJson.h>
 #define trigPin 5
 #define echoPin 6
 #define carPark1 2
@@ -80,8 +80,10 @@ void parkingInfo() {
   int parkingSpaces = 4;
   int emptySpaces = 0;
   int carCounter = 0, slot1 = 0, slot2 = 0, slot3 = 0, slot4 = 0;
+  String space1 = "empty", space2 = "empty", space3 = "empty", space4 = "empty";
   String carParked = "", availableSpaces = "";
   int distance;
+  String output;
 
   int carStatus1 = digitalRead(carPark1);
   int carStatus2 = digitalRead(carPark2);
@@ -90,61 +92,71 @@ void parkingInfo() {
 
   if (carStatus1 == 0) {
     slot1 = 1;
-    softSerial.println("Slot 1 is occupied");
-    Serial.println("Slot 1 is occupied");
+    space1 = "occupied";
+    // softSerial.println("Slot 1 is occupied");
   }
   else {
     slot1 = 0;
-    softSerial.println("Slot 1 is empty!");
-    Serial.println("Slot 1 is empty!");
+    space1 = "empty";
+    //softSerial.println("Slot 1 is empty!");
   }
   if (carStatus2 == 0) {
     slot2 = 1;
-    softSerial.println("Slot 2 is occupied");
-    Serial.println("Slot 2 is occupied");
+    space2 = "occupied";
+    //softSerial.println("Slot 2 is occupied");
   }
   if (carStatus2 == 1) {
     slot2 = 0;
-    softSerial.println("Slot 2 is empty!");
-    Serial.println("Slot 2 is empty!");
+    space2 = "empty";
+    //softSerial.println("Slot 2 is empty!");
   }
   if (carStatus3 == 0) {
     slot3 = 1;
-    softSerial.println("Slot 3 is occupied");
-    Serial.println("Slot 3 is occupied");
+    space3 = "occupied";
+    //softSerial.println("Slot 3 is occupied");
   }
   else {
     slot3 = 0;
-    softSerial.println("Slot 3 is empty!");
-    Serial.println("Slot 3 is empty!");
+    space3 = "empty";
+    //softSerial.println("Slot 3 is empty!");   
   }
 
   if (distance > 6) {
     slot4 = 0;
-    softSerial.println("Slot 4 is empty!");
-    Serial.println("Slot 4 is empty!");
+    space4 = "empty";
+    //softSerial.println("Slot 4 is empty!");
   }
 
   if (distance < 5) {
     slot4 = 1;
-    softSerial.println("Slot 4 is occupied!");
-    Serial.println("Slot 4 is occupied!");
+    space4 = "occupied";
+    //softSerial.println("Slot 4 is occupied!");
   }
 
   carCounter = slot1 + slot2 + slot3 + slot4;
-
   emptySpaces = parkingSpaces - carCounter;
   
-  availableSpaces = String(emptySpaces) + String("\n");
-  carParked = String(carCounter) + String("\n");
-  softSerial.print("Empty Spaces: ");
-  Serial.print("Empty Spaces: ");
-  softSerial.print(availableSpaces);
-  Serial.print(availableSpaces);
-  softSerial.print("Cars parked: ");
-  Serial.print("Cars parked: ");
-  softSerial.print(carParked);
-  Serial.print(carParked);
+  JsonDocument doc;
+
+  doc["empty Spaces"] = emptySpaces;
+  doc["cars parked"] = carCounter;
+  doc["slot 1"] = space1;
+  doc["slot 2"] = space2;
+  doc["slot 3"] = space3;
+  doc["slot 4"] = space4;
+
+  doc.shrinkToFit();  // optional
+
+  serializeJson(doc, output);
+
+  // availableSpaces = String(emptySpaces) + String("\n");
+  // carParked = String(carCounter) + String("\n");
+  // softSerial.print("Empty Spaces: ");
+  // softSerial.print(availableSpaces);
+  // softSerial.print("Cars parked: ");
+  // softSerial.print(carParked);
+  softSerial.println(output);
+  Serial.println(output);
   delay(100);
   
 
